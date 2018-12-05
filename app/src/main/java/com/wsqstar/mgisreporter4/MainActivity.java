6348211a70596000c36528f5618847988add30f9
wsq_star <wsq_star@outlook.com>
2018-12-05 19:48:45 +0800

    Update MainActivity.java
    http://lbsyun.baidu.com/index.php?title=androidsdk/guide/interaction/event 事件交互很重要

--- app/src/main/java/com/wsqstar/mgisreporter4/MainActivity.java
+++ app/src/main/java/com/wsqstar/mgisreporter4/MainActivity.java
@@ -209,9 +209,6 @@ public class MainActivity extends AppCompatActivity {
 //结束菜单栏目的设置以及功能管理
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
     //实现地图生命周期管理
     @Override
     protected void onResume() {
